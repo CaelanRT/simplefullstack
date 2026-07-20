8e8c844b96c7aa828464253963a214ce0ab04e97
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-07-20 14:11:00 -0400

    importing middleware

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
+// imports
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors")
 
+// create the app
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// port
 const port = process.env.port || 3000;
 
+// main entry route
 app.get("/", (req, res) => {
     res.send('hello world!');
 })
 
+// server listener
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}...`);
 })
