87c33427134aaf5f4e471b200facedaf65483960
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-07-20 21:53:27 -0400

    got clerk to work!

---
+++ frontend/components/SecretDashboard.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+const axios = require('axios');
+
+
+const SecretDashboard = () => {
+    const [data, setData] = useState({});
+
+
+
+  return (
+    <div>SecretDashboard</div>
+  )
+}
+export default SecretDashboard
